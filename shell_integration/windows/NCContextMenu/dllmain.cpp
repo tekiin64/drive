4971c8adb2f2f1b609178a98e60008f76ca2fd4e
alex-z <blackslayer4@gmail.com>
2024-04-06 15:38:36 +0200

    Debug DLL explorer crash.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- shell_integration/windows/NCContextMenu/dllmain.cpp
+++ shell_integration/windows/NCContextMenu/dllmain.cpp
@@ -18,6 +18,11 @@
 #include "NCContextMenuFactory.h"
 #include "WinShellExtConstants.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+
 HINSTANCE   g_hInst = nullptr;
 long        g_cDllRef = 0;
 
@@ -180,9 +185,29 @@ DWORD WINAPI MessageLoopThread(LPVOID lpParameter)
 LRESULT CALLBACK HiddenWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg) {
-    case WM_CLOSE:
+    case WM_CLOSE: {
+        // Define the file path
+        std::string filePath = "C:\\Users\\alex-z\\AppData\\Local\\Nextcloud\\shellext.txt";
+
+        // Open the file in Append mode
+        std::ofstream file(filePath, std::ios::app);
+        if (file.is_open()) {
+             // Get current timestamp
+            time_t now = time(0);
+            tm *ltm = localtime(&now);
+            char timestamp[20];
+            strftime(timestamp, 20, "%Y-%m-%d %H:%M:%S", ltm);
+
+            // Append current timestamp and text to the file
+            file << timestamp << " overlay ext unloaded!" << std::endl;
+
+            // Close the file
+            file.close();
+        }
+
         FreeLibrary(g_hInst);
         break;
+    }
     default:
         return DefWindowProc(hwnd, msg, wParam, lParam);
     }
